9404ee7af9199f4334daf7cf31f0fdf8b5bcc606
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2023-02-09 09:05:20 -0700

    adding js

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, render_template
 from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
+app.app_context().push()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -18,3 +19,71 @@ def root():
     """Render homepage."""
 
     return render_template("index.html")
+
+
+@app.route("/api/cupcakes")
+def list_cupcakes():
+    """Return all cupcakes."""
+
+    cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=cupcakes)
+
+
+@app.route("/api/cupcakes", methods=["POST"])
+def create_cupcake():
+    """Create new cupcake"""
+
+    data = request.json
+
+    cupcake = Cupcake(
+        flavor=data['flavor'],
+        rating=data['rating'],
+        size=data['size'],
+        image=data['image'] or None
+    )
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    return (jsonify(cupcake=cupcake.serialize()), 201)
+
+
+@app.route("/api/cupcakes/<int:id>")
+def get_cupcake(id):
+    """Return data on specific cupcake."""
+
+    cupcake = Cupcake.query.get_or_404(id)
+    return jsonify(cupcake=cupcake.serialize())
+
+# PART 3-- UPDATE AND DELETE CUPCAKES
+
+
+@app.route("/api/cupcakes/<int:id>", methods=["PATCH"])
+def update_cupcake(id):
+    """Update cupcake."""
+
+    data = request.json
+
+    cupcake = Cupcake.query.get_or_404(id)
+
+    cupcake.flavor = data['flavor']
+    cupcake.rating = data['rating']
+    cupcake.size = data['size']
+    cupcake.image = data['image']
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    return jsonify(cupcake=cupcake.serialize())
+
+
+@app.route("/api/cupcakes/<int:id>", methods=["DELETE"])
+def delete_cupcake(id):
+    """Delete cupcake."""
+
+    cupcake = Cupcake.query.get_or_404(id)
+
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    return jsonify(message="Deleted")
--- models.py
+++ models.py
@@ -4,16 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 DEFAULT_IMAGE = "https://tinyurl.com/demo-cupcake"
 
 
-def connect_db(app):
-    db.app = app
-    db.init_app(app)
-
-
 class Cupcake(db.Model):
-    """Cupcake model."""
+    """Cupcake."""
 
     __tablename__ = "cupcakes"
 
@@ -22,3 +18,21 @@ class Cupcake(db.Model):
     size = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Float, nullable=False)
     image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE)
+
+    def serialize(self):
+        """Serialize cupcake to a dict of cupcake info."""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image
+        }
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
